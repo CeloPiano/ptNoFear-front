aa19f3c988b855e46944df6e21d6c6f5733e3458
Arkhrer <jonasrngl@gmail.com>
2022-11-18 18:17:03 -0300

    Fix: Login page positioning and plus button details

--- src/Components/PlusButton.tsx
+++ src/Components/PlusButton.tsx
@@ -1,9 +1,11 @@
 const PlusButton = () => {
     return (
         <>
-            <button className = "box-content w-[71px] h-[71px] border-4 bg-button-color rounded-full place-content-center">
-                <div className = "box-content bg-white w-[22px] h-[2px] relative top-[16%] left-[34.51%]" />
-                <div className = "box-content bg-white w-[2px] h-[22px] relative top-[0%] left-[49%]" />
+            <button className = "box-content w-[71px] h-[71px] bg-button-color rounded-full flex justify-center drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
+                <div className="flex justify-center items-center">
+                    <div className = "box-content bg-white w-[22px] h-[2px] absolute flex justify-center" />
+                    <div className = "box-content bg-white w-[2px] h-[22px] absolute flex justify-center" />
+                </div>
             </button>
         </>
     )
--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,13 +4,18 @@ import PLACEHOLDER from '../Assets/PLACEHOLDER.png'
 function Login() {
 
     return(
-        <div>
-          <img src = {PLACEHOLDER} alt="Placeholder" className="absolute top-[180px] left-[435px]"/>
-          <h1 className="font-['Inter'] text-[100px] font-[100] italic text-center absolute top-[631px] left-[581px]">
+      
+      <div className="flex items-center justify-center pt-36">
+        <div className = "flex flex-col">
+          <img src = {PLACEHOLDER} alt="Placeholder" className=""/>
+          <h1 className="font-['Inter'] text-[100px] font-[100] italic text-center pt-0">
             SOPT
           </h1>
-          <button className="font-['Inter'] text-[20px] font-[400] text-center text-white absolute top-[791px] left-[569px] px-7 py-2 box-content bg-button-color rounded-md">Entrar com o Google</button>
+          <div className="flex items-center justify-center pt-6">
+            <button className="font-['Inter'] text-[20px] font-[400] text-center text-white px-8 py-2 box-content bg-button-color rounded-md">Entrar com o Google</button>
+          </div>
         </div>
+      </div>
     )
 }
 
