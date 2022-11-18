b5639572f5a21f6eedd12286871adb011775392f
Arkhrer <jonasrngl@gmail.com>
2022-11-18 10:22:09 -0300

    Setup: Fixing tailwind setup

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,9 @@ function Home() {
 
     return(
         <div>
-          Vamo nessa!
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
         </div>
     )
 }
