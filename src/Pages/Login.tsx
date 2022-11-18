ac1fe58e3922303b078dae1dc554931102698538
Arkhrer <jonasrngl@gmail.com>
2022-11-18 15:20:13 -0300

    Feature: Login Page Initial Draft

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,16 @@
+import PlusButton from '../Components/PlusButton'
+
+function Login() {
+
+    return(
+        <div>
+          <img src = "http://cdn.thinglink.me/api/image/832723959517544448/1024/10/scaletowidth/0/0/1/1/false/true?wait=true" alt="Placeholder" className="absolute top-[80px] left-[330px]"/>
+          <h1 className="font-['Inter'] text-[100px] font-[100] italic text-center absolute top-[631px] left-[581px]">
+            SOPT
+          </h1>
+          <button className="font-['Inter'] text-[20px] font-[400] text-center text-white absolute top-[791px] left-[569px] px-7 py-2 box-content bg-button-color rounded-md">Entrar com o Google</button>
+        </div>
+    )
+}
+
+export default Login
