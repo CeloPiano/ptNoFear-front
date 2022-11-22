9857a3ef2c2b2a50acc57c8045226f618ed0db11
Arkhrer <jonasrngl@gmail.com>
2022-11-22 18:51:57 -0300

    feat: Introduced definitive image and fonts

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,5 +1,4 @@
-import PlusButton from '../Components/PlusButton'
-import PLACEHOLDER from '../Assets/PLACEHOLDER.png'
+import Classroom from '../Assets/Classroom.png'
 
 function Login() {
 
@@ -7,12 +6,12 @@ function Login() {
       
       <div className="flex items-center justify-center pt-36">
         <div className = "flex flex-col">
-          <img src = {PLACEHOLDER} alt="Placeholder" className=""/>
-          <h1 className="font-['Inter'] text-[100px] font-[100] italic text-center pt-0">
+          <img src = {Classroom} alt="Classroom" className=""/>
+          <h1 className="font-inter text-[100px] font-[100] italic text-center pt-0">
             SOPT
           </h1>
           <div className="flex items-center justify-center pt-6">
-            <button className="font-['Inter'] text-[20px] font-[400] text-center text-white px-8 py-2 box-content bg-button-color rounded-md">Entrar com o Google</button>
+            <button className="font-inter text-[20px] font-[400] text-center text-white px-7 py-1 box-content bg-button-color rounded-md">Entrar com o Google</button>
           </div>
         </div>
       </div>
